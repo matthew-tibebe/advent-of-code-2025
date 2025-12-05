63b7303682d77cbec609b81ec6759bfa1b664486
Matthew Tibebe <tibebematthew@yahoo.com>
2025-12-05 17:54:37 -0500

    day 2 part 2 solution implemented (slow)

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //d1p1();
         //d1p2();
-        d2p1();
+        //d2p1();
+        d2p2();
     }
 
     public static void d1p1() throws IOException {
@@ -89,7 +90,7 @@ public class Main {
         System.out.printf("zero counter = %d\n", zeroCounter);
     }
 
-    //todo there is definitely a faster way to do this
+    //todo there is definitely a faster way to do these
     public static void d2p1() throws IOException {
         BufferedReader fileReader = new BufferedReader(new FileReader("data/day-2-input.txt"));
         String input = fileReader.readLine();
@@ -109,6 +110,47 @@ public class Main {
                 }
             }
         }
+        for(int i = 0; i < invalidIds.size(); i++){
+            result+=invalidIds.get(i);
+        }
+        System.out.println("result is: " + result);
+    }
+
+    public static void d2p2() throws IOException {
+        BufferedReader fileReader = new BufferedReader(new FileReader("data/day-2-input.txt"));
+        String input = fileReader.readLine();
+        String[] ids = input.split(",");
+        long result = 0;
+        ArrayList<Long> invalidIds = new ArrayList<>();
+        for(int i = 0; i < ids.length; i++){
+            String idRange = ids[i];
+            int dashIndex = idRange.indexOf("-");
+            long startId = Long.parseLong(idRange.substring(0,dashIndex));
+            long endId = Long.parseLong(idRange.substring(dashIndex+1));
+            for(long j = startId; j <= endId; j++){
+                String id = String.valueOf(j);
+                for(int k = 0; k < (id.length()/2)+1; k++){
+                    String subsequence = id.substring(0,k+1);
+                    int repeatCounter = 0;
+                    if(id.length()%subsequence.length() == 0) {
+                        for (int l = 0; l < id.length() - k; l += (k + 1)) {
+                            String currentSequence = id.substring(l, l + k + 1);
+                            System.out.println("id: " + id + " subseq " + subsequence + " cseq " + currentSequence);
+                            if (currentSequence.equals(subsequence)) {
+                                repeatCounter++;
+                            } else {
+                                repeatCounter = 0;
+                                break;
+                            }
+                        }//close L
+                    }
+                    if(repeatCounter > 1){
+                        invalidIds.add(Long.parseLong(id));
+                        break;
+                    }
+                }//close K
+            }//close J
+        }//close I
         for(int i = 0; i < invalidIds.size(); i++){
             result+=invalidIds.get(i);
         }
