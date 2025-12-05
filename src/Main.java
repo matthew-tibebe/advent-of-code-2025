1614ea465c939c60663683ec421d90ab640b726d
Matthew Tibebe <tibebematthew@yahoo.com>
2025-12-05 14:07:04 -0500

    day 1 part 2 done, will make more elegant/efficient later maybe

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        dayOne();
+        //d1p1();
+        d1p2();
     }
 
-    public static void dayOne() throws IOException {
+    public static void d1p1() throws IOException {
         int minVal = 0;
         int maxVal = 99;
         int currentVal = 50;
@@ -35,7 +36,7 @@ public class Main {
                 currentVal = maxVal+currentVal+1;
             }
             if(currentVal > maxVal){ //100 should be 0
-                currentVal%=100;
+                currentVal%=(maxVal+1);
             }
             System.out.printf("value after correction %d\t", currentVal);
             if(currentVal == 0){
@@ -45,4 +46,47 @@ public class Main {
         }
         System.out.printf("zero counter = %d\n",zeroCounter);
     }
+
+    ///todo figure out more elegant solution later
+    public static void d1p2() throws IOException {
+        int minVal = 0;
+        int maxVal = 99;
+        int currentVal = 50;
+        int zeroCounter = 0;
+        BufferedReader fileReader = new BufferedReader(new FileReader(new File("data/day-1-input.txt")));
+        ArrayList<String> rotations = new ArrayList<>();
+        String line = fileReader.readLine();
+        while(line != null){
+            rotations.add(line);
+            line = fileReader.readLine();
+        }
+        fileReader.close();
+        for(int i = 0; i < rotations.size(); i++){
+            String step = rotations.get(i);
+            String direction = step.substring(0,1);
+            int amount = Integer.parseInt(step.substring(1));
+            System.out.printf("step: %s\t %d->", step, currentVal);
+            while(amount > 0){
+                if(direction.equals("L")){
+                    currentVal--;
+                } else {
+                    currentVal++;
+                }
+                if(currentVal < minVal){
+                    currentVal = maxVal;
+                }
+                if(currentVal > maxVal){
+                    currentVal = minVal;
+                }
+                if(currentVal == 0){
+                    zeroCounter++;
+                }
+                amount--;
+            }
+            System.out.printf("%d\t zc: %d\n", currentVal, zeroCounter);
+        }
+        System.out.printf("zero counter = %d\n", zeroCounter);
+    }
+
+
 }
